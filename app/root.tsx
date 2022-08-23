a665e5c16a28bc6ba6afe09ae5e44fe50ae8750f
Alvin Fletcher <alvin@modern.kiwi>
2022-08-23 14:37:08 +1200

    Updated Stylesheet code.

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 import type { LinksFunction, MetaFunction } from '@remix-run/node';
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
-import rootStylesheet from './root.css';
+import styles from '~/styles/root.css';
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
@@ -9,7 +9,7 @@ export const meta: MetaFunction = () => ({
 });
 
 export const links: LinksFunction = () => {
-  return [{ rel: 'stylesheet', href: rootStylesheet }];
+  return [{ rel: 'stylesheet', href: styles }];
 };
 
 export default function App() {
