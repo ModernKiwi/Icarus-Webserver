fa9fa47bdde27475d651d6fe38609776a2cb9213
Alvin Fletcher <alvin@modern.kiwi>
2022-10-10 19:37:31 +1300

    Implimented Registration code, Updated form.

--- app/routes/__authentication/register.tsx
+++ app/routes/__authentication/register.tsx
@@ -1,27 +1,88 @@
 import type { ActionFunction, LoaderFunction, MetaFunction } from '@remix-run/node';
 import { json, redirect } from '@remix-run/node';
 import { Form, Link, useActionData, useSearchParams } from '@remix-run/react';
-import * as React from 'react';
+import validator from 'validator';
+import { checkEmailExists, checkUsernameExists, createUser } from '~/database/userdb.server';
+import { createUserSession, getUsername } from '~/sessions/userSession.server';
+import { safeRedirect } from '~/utils';
+
+interface IFormErrors {
+  username?: string;
+  email?: string;
+  password?: string;
+}
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const username = await getUsername(request);
+  if (username) return redirect('/');
+  return json({});
+};
+
+export const action: ActionFunction = async ({ request }: { request: Request }) => {
+  const formData = await request.formData();
+  const username = formData.get('username')?.toString();
+  const email = formData.get('email')?.toString();
+  const password = formData.get('password')?.toString();
+  const passwordCopy = formData.get('password-confirm')?.toString();
+  const remember = formData.get('remember-me')?.toString();
+  const redirectTo = safeRedirect(formData.get('redirectTo'), '/');
+
+  // Verify required data
+  if (!username) return json({ errors: { username: 'Username Missing' } }, { status: 400 });
+  if (!password) return json({ errors: { password: 'Password Missing' } }, { status: 400 });
+  if (!passwordCopy) return json({ errors: { password: 'Password Missing' } }, { status: 400 });
+
+  //  Check if valid details
+  if (!validator.isAlphanumeric(username))
+    return json({ errors: { username: 'Username is not Alphanumeric' } }, { status: 400 });
+  if (await checkUsernameExists(username))
+    return json({ errors: { username: 'Username already taken' } }, { status: 400 });
+  if (email && !validator.isEmail(email))
+    return json({ errors: { email: 'Email is not valid' } }, { status: 400 });
+  if (email && (await checkEmailExists(email)))
+    return json({ errors: { email: 'Email already taken' } }, { status: 400 });
+  if (password !== passwordCopy)
+    return json({ errors: { password: 'Passwords did not match' } }, { status: 400 });
+
+  const user = await createUser(username, password, email);
+
+  return createUserSession(
+    user.username,
+    user.account?.admin || false,
+    remember == 'on' ? true : false,
+    redirectTo
+  );
+};
 
 export default function RegisterPage() {
   const [searchParams] = useSearchParams();
-  const redirectTo = searchParams.get('redirectTo') || '/dashboard';
-  const emailRef = React.useRef<HTMLInputElement>(null);
-  const passwordRef = React.useRef<HTMLInputElement>(null);
+  const redirectTo = searchParams.get('redirectTo') || '/';
 
   return (
-    <div className=''>
+    <div className='devIndicator'>
       <Form method='post' className=''>
         <div className=''>
+          <div className=''>
+            <label htmlFor='username' className=''>
+              Username
+            </label>
+            <input
+              id='username'
+              required
+              autoFocus={true}
+              name='username'
+              type='text'
+              aria-describedby='username-error'
+              placeholder='Uername'
+              className=''
+            />
+          </div>
           <div className=''>
             <label htmlFor='email' className=''>
               Email address
             </label>
             <input
-              ref={emailRef}
               id='email'
-              required
-              autoFocus={true}
               name='email'
               type='email'
               autoComplete='email'
@@ -36,7 +97,6 @@ export default function RegisterPage() {
             </label>
             <input
               id='password'
-              ref={passwordRef}
               name='password'
               type='password'
               autoComplete='new-password'
@@ -52,7 +112,6 @@ export default function RegisterPage() {
             </label>
             <input
               id='password-confirm'
-              ref={passwordRef}
               name='password-confirm'
               type='password'
               autoComplete='new-password'
