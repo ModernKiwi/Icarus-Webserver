2e2e150885c0a2ad0ee9ba3eee23deae605e5dee
Alvin Fletcher <alvin@modern.kiwi>
2022-10-10 16:00:56 +1300

    Removed util dupe. removed util import from login.

--- app/routes/__authentication/login.tsx
+++ app/routes/__authentication/login.tsx
@@ -2,7 +2,6 @@ import type { ActionFunction, LoaderFunction, MetaFunction } from '@remix-run/no
 import { json, redirect } from '@remix-run/node';
 import { Form, Link, useActionData, useSearchParams } from '@remix-run/react';
 import * as React from 'react';
-import { safeRedirect, validateEmail } from '~/utils';
 
 export const meta: MetaFunction = () => {
   return {
